38130c6f3d3c28d237f16e9ac7cf9f65972ef515
nxrohan <jhkim@coasia.com>
2022-06-15 13:08:03 +0900

    board: rohan: move inittab to skeleton/etc
    Signed-off-by: nxrohan <jhkim@coasia.com>

--- board/rohan/post-image.sh
+++ board/rohan/post-image.sh
@@ -1,18 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+set -ue
 
 BOARD_DIR="$(dirname $0)"
+SKELETON=${BOARD_DIR}/skeleton
 
-if grep -Eq "^BR2_TARGET_ROOTFS_CPIO=y$" ${BR2_CONFIG}; then
-        if [ ! -e ${TARGET_DIR}/init ]; then
-                install -m 0755 fs/cpio/init ${TARGET_DIR}/init;
-        fi
-        fakeroot mknod -m 0622 ${TARGET_DIR}/dev/console c 5 1
-fi
-
-if grep -Eq "^BR2_TARGET_GRUB2=y$" ${BR2_CONFIG}; then
-        cp -f ${BOARD_DIR}/grub.cfg ${BINARIES_DIR}/efi-part/EFI/BOOT/grub.cfg
-fi
-
-if ! grep -Eq "^BR2_TARGET_GENERIC_GETTY=y$" ${BR2_CONFIG}; then
-        cp -f ${BOARD_DIR}/inittab ${TARGET_DIR}/etc/inittab
-fi
+source ${BOARD_DIR}/post-system.sh
+source ${BOARD_DIR}/post-adb.sh
